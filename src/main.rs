0619e2a03c9b8558f3f3cc161fa7fe26fe5812c6
kiy <binary.kiy.tree@gmail.com>
2023-05-23 00:02:56 +0900

    Config構造体の適用

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod config;
 
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io;
-use std::io::{BufRead, Read, Write};
+use std::io::{BufRead, Read};
 use std::path::Path;
+use crate::config::Config;
 
 fn main() {
     println!("Enter file path to compile:");
@@ -13,10 +14,8 @@ fn main() {
     file_name.retain(|c| c != '\r' && c != '\n' && c != '"');
     let source = read_all(&file_name)
         .expect("Failed to open the file to compile.");
-    let mut out = OpenOptions::new().create(true).truncate(true).write(true)
-        .open(Path::new(&file_name).parent().unwrap().join("out.html"))
-        .expect("Failed to create the output file");
-    parse_and_write(source, &mut out);
+    let mut config = Config::init(file_name);
+    parse_and_write(source, &mut config);
 }
 
 fn read_all<S: AsRef<str>>(name: S) -> io::Result<String> {
@@ -27,30 +26,25 @@ fn read_all<S: AsRef<str>>(name: S) -> io::Result<String> {
     Ok(input)
 }
 
-fn write_all<S: AsRef<str>>(content: S, file: &mut File) {
-    file.write_all(content.as_ref().as_bytes())
-        .expect("Failed to write to the output file");
-}
-
-fn parse_and_write(source: String, out: &mut File) {
+fn parse_and_write(source: String, config: &mut Config) {
     let mut source = source.as_str();
     loop {
         if let Some((before_lhc_comment, other)) = source.split_once("<!--?") {
-            write_all(before_lhc_comment, out);
+            config.write_all(before_lhc_comment);
             let (lhc_comment, after_lhc_comment) = other.split_once("-->")
                 .expect("Syntax Error: expected -->, found eof");
-            lhc_process(lhc_comment, out);
+            lhc_process(lhc_comment, config);
             source = after_lhc_comment;
         } else {
-            write_all(source, out);
+            config.write_all(source);
             break;
         }
     }
 }
 
-fn lhc_process(content: &str, out: &mut File) {
+fn lhc_process(content: &str, config: &mut Config) {
     if content.is_empty() {
-        write_all("<!--?-->", out);
+        config.write_all("<!--?-->");
     }
     let (key, args): (& str, & str) = content.split_once(' ').unwrap_or((content, ""));
     let args_itr = args.split(' ').filter(|arg| {
@@ -64,16 +58,16 @@ fn lhc_process(content: &str, out: &mut File) {
     });
     match key {
         "include" => {
-            include(args_itr, out);
+            include(args_itr, config);
         }
         _ => {
             eprintln!("Unknown lhc comment: {}", key);
-            write_all("<!--?-->", out);
+            config.write_all("<!--?-->");
         }
     }
 }
 
-fn include<'a, F: Iterator<Item=(&'a str, &'a str)>>(iter: F, out: &mut File) {
+fn include<'a, F: Iterator<Item=(&'a str, &'a str)>>(iter: F, config: &mut Config) {
     for (key, value) in iter {
         match key {
             "link" => {
@@ -87,7 +81,7 @@ fn include<'a, F: Iterator<Item=(&'a str, &'a str)>>(iter: F, out: &mut File) {
                         .expect("[ERROR] Expected '</body>', not found");
                     source = body.to_string();
                 }
-                parse_and_write(source, out);
+                parse_and_write(source, config);
             }
             _ => {
                 eprintln!("[WARN] Unknown property for \"include\": \"{}={}\"", key, value);
