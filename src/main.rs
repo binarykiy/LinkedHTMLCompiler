d7f4c0f7a118b17f94d7a87c869fda241ac586f1
kiy <binary.kiy.tree@gmail.com>
2023-05-22 22:43:45 +0900

    <!--?include link="example.html"-->の実装、未デバッグ

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,17 @@ fn lhc_process(content: &str, out: &mut File) {
             for (key, value) in args_itr {
                 match key {
                     "link" => {
-                        todo!("open {} and read it", value);
+                        let link = value.trim_matches('"');
+                        let mut source = read_all(link)
+                            .expect(format!("[ERROR] Failed to read the linked file: {}", value).as_str());
+                        if let Some((_, body_from_prefix)) = source.split_once("<body") {
+                            let (_, body_after_prefix) = body_from_prefix.split_once('>')
+                                .expect("[ERROR] Expected '>', not found");
+                            let (_, body) = body_after_prefix.split_once("</body>")
+                                .expect("[ERROR] Expected '</body>', not found");
+                            source = body.to_string();
+                        }
+                        parse_and_write(source, out);
                     }
                     _ => {
                         eprintln!("[WARN] Unknown property for \"include\": \"{}={}\"", key, value);
