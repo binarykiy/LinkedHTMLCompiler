3b4cc247530e8b35fcc1caa432e7189da0f56c9e
binarykiy <binary.kiy.tree@gmail.com>
2023-05-23 14:10:41 +0900

    extractをincludeに適用

--- src/tag/include.rs
+++ src/tag/include.rs
@@ -1,21 +1,27 @@
 use crate::config::Config;
 use crate::{parse_and_write, read_all};
+use crate::tag::{extract, SyntaxError};
 
 pub fn run<'a, F: Iterator<Item=(&'a str, &'a str)>>(iter: F, config: &mut Config) {
     for (key, value) in iter {
         match key {
             "link" => {
                 let link = value.trim_matches('"');
-                let mut source = read_all(config.relative_path(link))
+                let source = read_all(config.relative_path(link))
                     .expect(format!("[ERROR] Failed to read the linked file: {}", value).as_str());
-                if let Some((_, body_from_prefix)) = source.split_once("<body") {
-                    let (_, body_after_prefix) = body_from_prefix.split_once('>')
-                        .expect("[ERROR] Expected '>', not found");
-                    let (body, _) = body_after_prefix.split_once("</body>")
-                        .expect("[ERROR] Expected '</body>', not found");
-                    source = body.to_string();
+                match extract(source.as_str(), "body") {
+                    Ok(body) => {
+                        parse_and_write(body.to_string(), config);
+                    }
+                    Err(e) => {
+                        if let SyntaxError::NoTagBegin(_) = e {
+                            parse_and_write(source, config);
+                        } else {
+                            eprintln!("[FATAL] {}", e);
+                            panic!()
+                        }
+                    }
                 }
-                parse_and_write(source, config);
             }
             _ => {
                 eprintln!("[WARN] Unknown property for \"include\": \"{}={}\"", key, value);
