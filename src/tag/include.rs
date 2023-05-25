ee01cc197f41591c5d81bb4da08844b561254075
binarykiy <binary.kiy.tree@gmail.com>
2023-05-25 16:41:26 +0900

    includeをParse対応

--- src/tag/include.rs
+++ src/tag/include.rs
@@ -1,26 +1,53 @@
+use std::collections::VecDeque;
 use crate::config::Config;
-use crate::{parse_and_write, read_all};
-use crate::util::{extract, ParsedTag, ParsedText, SyntaxError};
+use crate::{parse, read_all};
+use crate::util::{ParsedTag, ParsedText};
 
 pub fn run<'a>(mut source: ParsedTag<'a>, config: &mut Config) -> Option<Vec<ParsedText<'a>>> {
+    let mut res = None;
     source.consume("link", |value| {
         let link = value.trim_matches('"');
         let source = read_all(config.relative_path(link))
             .expect(format!("[ERROR] Failed to read the linked file: {}", value).as_str());
-        // todo: use parse()
-        match extract(source.as_str(), "body") {
-            Ok(body) => {
-                parse_and_write(body.to_string(), config);
-            }
-            Err(e) => {
-                if let SyntaxError::NoTagBegin(_) = e {
-                    parse_and_write(source, config);
-                } else {
-                    eprintln!("[FATAL] {}", e);
-                    panic!()
+        let parsed = parse(source, config);
+        let len = parsed.len();
+        let mut begin = len;
+        let mut end = len;
+        for i in 0..len {
+            if let ParsedText::Tag(tag) = &parsed[i] {
+                if tag.tag() == "body" {
+                    if begin == len {
+                        begin = i;
+                    } else {
+                        eprintln!("[ERROR] Duplicate <body> tags found");
+                        return;
+                    }
+                }
+                if tag.tag() == "/body" {
+                    if end == len {
+                        end = i;
+                    } else {
+                        eprintln!("[ERROR] Duplicate </body> tags found");
+                        return;
+                    }
                 }
             }
         }
+        if begin == len && end == len {
+            res = Some(parsed);
+            return;
+        }
+        if begin != len && end != len && begin < end {
+            let mut tmp = VecDeque::from(parsed);
+            for _ in 0..=begin {
+                tmp.pop_front();
+            }
+            for _ in end..len {
+                tmp.pop_back();
+            }
+            res = Some(Vec::from(tmp));
+            return;
+        }
     });
-    None
+    res
 }
