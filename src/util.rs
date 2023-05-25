4c66291ae94ad158eaf4bc76615d5cb03a7b8a20
binarykiy <binary.kiy.tree@gmail.com>
2023-05-25 16:55:08 +0900

    不要なSyntaxErrorとextract()削除

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::error::Error;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 
@@ -190,49 +189,5 @@ impl<'a> Display for ParsedTag<'a> {
             buf += value;
         }
         write!(fmt, "{}", buf)
-    }
-}
-
-#[derive(Debug)]
-pub enum SyntaxError {
-    NoTagBegin(String),
-    NoTagEnd(String),
-    IsNotClosed(String),
-}
-
-impl Display for SyntaxError {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::NoTagBegin(s) => {
-                write!(fmt, "Expected {}, not found", s)
-            }
-            Self::NoTagEnd(s) => {
-                write!(fmt, "Expected {}, not found", s)
-            }
-            Self::IsNotClosed(s) => {
-                write!(fmt, "Expected > for {}, not found", s)
-            }
-        }
-    }
-}
-
-impl Error for SyntaxError {
-}
-
-pub fn extract<'a, 'b>(source: &'a str, tag: &'b str) -> Result<&'a str, SyntaxError> {
-    let opened_begin = format!("<{}", tag);
-    if let Some((_, from_prefix)) = source.split_once(&opened_begin) {
-        if let Some((_, after_prefix)) = from_prefix.split_once('>') {
-            let end = format!("</{}>", tag);
-            if let Some((res, _)) = after_prefix.rsplit_once(&end) {
-                Ok(res)
-            } else {
-                Err(SyntaxError::NoTagEnd(end))
-            }
-        } else {
-            Err(SyntaxError::IsNotClosed(opened_begin))
-        }
-    } else {
-        Err(SyntaxError::NoTagBegin(opened_begin))
     }
 }
