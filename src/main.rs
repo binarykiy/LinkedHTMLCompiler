85271da761118f102748dfe453c91644ebbf59a3
kiy <binary.kiy.tree@gmail.com>
2023-05-27 17:29:25 +0900

    コンパイル時間のタイマー作成

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod util;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
+use std::time::Instant;
 use crate::config::Config;
 
 fn main() {
@@ -13,10 +14,13 @@ fn main() {
     let mut name = String::new();
     io::stdin().lock().read_line(&mut name)
         .expect("Failed to read the input");
+    println!("[INFO] Compilation started.");
+    let timer = Instant::now();
     name.retain(|c| c != '\r' && c != '\n' && c != '"');
     let mut cfg = Config::init(name.clone());
     let source = cfg.read_absolute(PathBuf::from(name))
         .expect("Failed to open the file to compile.");
     let doc = parse::parse(source, &mut cfg);
-    cfg.write_all(format!("{}", doc))
+    cfg.write_all(format!("{}", doc));
+    println!("[INFO] Compilation finished. Time = {:?}", timer.elapsed());
 }
