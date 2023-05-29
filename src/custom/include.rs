7d3d01736695589d6ab7e20a4777b6cc702ed214
kiy <binary.kiy.tree@gmail.com>
2023-05-29 14:09:04 +0900

    Tag::consumeをmove_valueに移行

--- src/custom/include.rs
+++ src/custom/include.rs
@@ -4,21 +4,18 @@ use crate::parse::doc::Doc;
 use crate::parse::tag::Tag;
 
 pub fn run(mut tag: Tag, cfg: &mut Config) -> Option<Doc> {
-    let mut doc = None;
-    tag.consume("link", |value| {
-        let link = value.trim_matches('"');
-        let source = cfg.read_relative(link)
-            .expect(format!("[ERROR] Failed to read the linked file: {}", value).as_str());
-        let mut linked_doc = parse::parse(source, cfg);
-        let begin = linked_doc.find_tags("body");
-        let end = linked_doc.find_tags("/body");
-        validate_body_tag(&begin, &end);
-        if begin.len() == 1 && begin.len() == 1 {
-            linked_doc.extract(begin[0]+1..end[0]);
-        }
-        doc = Some(linked_doc);
-    });
-    doc
+    let link_raw = tag.move_value("link")?;
+    let link = link_raw.trim_matches('"');
+    let source = cfg.read_relative(link)
+        .expect(format!("[ERROR] Failed to read the linked file: {}", link).as_str());
+    let mut linked_doc = parse::parse(source, cfg);
+    let begin = linked_doc.find_tags("body");
+    let end = linked_doc.find_tags("/body");
+    validate_body_tag(&begin, &end);
+    if begin.len() == 1 && begin.len() == 1 {
+        linked_doc.extract(begin[0]+1..end[0]);
+    }
+    Some(linked_doc)
 }
 
 fn validate_body_tag(begin: &Vec<usize>, end: &Vec<usize>) -> bool {
