a55e6a7ebff964fa48c7b765c81465a8e9722f4c
kiy <binary.kiy.tree@gmail.com>
2023-05-29 15:25:47 +0900

    include::runのlink欠落をWARN表示するように

--- src/custom/include.rs
+++ src/custom/include.rs
@@ -4,7 +4,11 @@ use crate::parse::doc::Doc;
 use crate::parse::tag::Tag;
 
 pub fn run(mut tag: Tag, cfg: &mut Config) -> Option<Doc> {
-    let link_raw = tag.consume("link")?;
+    let Some(link_raw) = tag.consume("link")
+        else {
+            eprintln!("[WARN] attribute 'link' is essential for ?include");
+            return None
+        };
     let link = link_raw.trim_matches('"');
     let source = cfg.read_relative(link)
         .expect(format!("[ERROR] Failed to read the linked file: {}", link).as_str());
