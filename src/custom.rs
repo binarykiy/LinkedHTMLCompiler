75734faf790554c4c99aac7942908f079f305459
kiy <binary.kiy.tree@gmail.com>
2023-05-29 13:59:50 +0900

    match文のcustom_tag呼び出しをVecDict経由に変更できるよう準備

--- src/custom.rs
+++ src/custom.rs
@@ -1,3 +1,19 @@
 mod include;
 
+use once_cell::sync::Lazy;
 pub use include::run as include;
+use crate::config::Config;
+use crate::parse::doc::Doc;
+use crate::parse::tag::Tag;
+use crate::util::VecDict;
+
+static CUSTOM_TAGS: Lazy<VecDict<&'static str, fn(Tag, &mut Config) -> Option<Doc>>> = Lazy::new(|| {
+    let mut dict: VecDict<&'static str, fn(Tag, &mut Config) -> Option<Doc>> = VecDict::new();
+    dict.push_unique("include", include::run);
+    dict
+});
+
+pub fn run(tag: Tag, config: &mut Config) -> Option<Doc> {
+    let func = CUSTOM_TAGS.get(&tag.tag())?;
+    func(tag, config)
+}
