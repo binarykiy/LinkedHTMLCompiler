959969319b581d576ba560b3ad9d3a0f05d680fa
kiy <binary.kiy.tree@gmail.com>
2023-05-29 20:28:13 +0900

    find_first_of, find_first_not_of, retain_from, validate_index 追加

--- src/source.rs
+++ src/source.rs
@@ -10,4 +10,33 @@ impl<'a> SourceManager<'a> {
             begin: 0,
         }
     }
+    pub fn find_first_of(&self, bytes: &[u8]) -> usize {
+        let len = bytes.len();
+        let to = self.source.len() - len;
+        for i in self.begin..=to {
+            if &self.source[i..=i + len] == bytes {
+                return i
+            }
+        }
+        to + 1
+    }
+    pub fn find_first_not_of(&self, bytes: &[u8]) -> usize {
+        let len = bytes.len();
+        let to = self.source.len() - len;
+        for i in self.begin..=to {
+            if &self.source[i..=i + len] != bytes {
+                return i
+            }
+        }
+        to + 1
+    }
+    pub fn retain_from(&mut self, from: usize) {
+        self.validate_index(from);
+        self.begin = from;
+    }
+    fn validate_index(&self, idx: usize) {
+        if idx > self.source.len() {
+            panic!("SourceManager: byte index {idx} is out of bounds")
+        }
+    }
 }
