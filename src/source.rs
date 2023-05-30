27250dca506455fdbb3402d0772fa0471952feed
binarykiy <binary.kiy.tree@gmail.com>
2023-05-30 13:34:14 +0900

    SourceManagerのインデックス系変数をfrom, end, next_fromに, from..endが分割前半, end..next_fromが分割列, next_from..が分割後半

--- src/source.rs
+++ src/source.rs
@@ -1,19 +1,23 @@
 pub struct SourceManager<'a> {
     source: &'a [u8],
-    begin: usize,
+    from: usize,
+    end: usize,
+    next_from: usize,
 }
 
 impl<'a> SourceManager<'a> {
     pub fn new(source: &'a String) -> Self {
         Self {
             source: source.as_bytes(),
-            begin: 0,
+            from: 0,
+            end: 0,
+            next_from: 0,
         }
     }
     pub fn find_first_of(&self, bytes: &[u8]) -> usize {
         let len = bytes.len();
         let to = self.source.len() - len;
-        for i in self.begin..=to {
+        for i in self.from..=to {
             if &self.source[i..=i + len] == bytes {
                 return i
             }
@@ -23,7 +27,7 @@ impl<'a> SourceManager<'a> {
     pub fn find_first_not_of(&self, bytes: &[u8]) -> usize {
         let len = bytes.len();
         let to = self.source.len() - len;
-        for i in self.begin..=to {
+        for i in self.from..=to {
             if &self.source[i..=i + len] != bytes {
                 return i
             }
@@ -32,7 +36,7 @@ impl<'a> SourceManager<'a> {
     }
     pub fn retain_from(&mut self, from: usize) {
         self.validate_index(from);
-        self.begin = from;
+        self.from = from;
     }
     fn validate_index(&self, idx: usize) {
         if idx > self.source.len() {
@@ -41,15 +45,15 @@ impl<'a> SourceManager<'a> {
     }
     pub fn pop_if_starts_with(&mut self, bytes: &[u8]) -> bool {
         let len = bytes.len();
-        if self.source.len() - self.begin < len {
+        if self.source.len() - self.from < len {
             return false
         }
-        for i in self.begin..len {
+        for i in self.from..len {
             if self.source[i] != bytes[i] {
                 return false
             }
         }
-        self.begin += len;
+        self.from += len;
         true
     }
 }
